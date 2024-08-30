5ec10c4923443b2c6faffa82627b50dd05793d2d
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-08-30 14:22:01 +0200

    Pointcut declaration for re-use

--- src/main/java/eu/kudljo/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/eu/kudljo/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,13 +2,19 @@ package eu.kudljo.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-    @Before("execution(* eu.kudljo.aopdemo.dao.*.*(..))")
+    @Pointcut("execution(* eu.kudljo.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {
+
+    }
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
     }
