4f5b920c57bbea2fdac1ac5a2bb63d130b684a44
Josef Kudláček <j.k.kudlacek@gmail.com>
2024-08-30 16:05:58 +0200

    JoinPoints - read method

--- src/main/java/eu/kudljo/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/eu/kudljo/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,9 +1,12 @@
 package eu.kudljo.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
 
 @Aspect
 @Component
@@ -11,8 +14,12 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     @Before("eu.kudljo.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
         System.out.println("\n====>>> Executing @Before advice on addAccount()");
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
     }
 
 }
